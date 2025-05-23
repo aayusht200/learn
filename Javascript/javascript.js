a1adf22010f7e6ccad14c5001481e2102c1359ea
aayusht200 <aayush.t200@gmail.com>
2025-05-22 22:09:45 -0400

    starting to learn javascript

---
+++ Javascript/javascript.js
@@ -0,0 +1,21 @@
+let name="Aayush";
+let surname="Trivedi";
+console.log(23+97);
+console.log(32+97);
+console.log(23+79);
+console.log(29+97);
+console.log(23+37);
+console.log((4+6+9)/77);
+let a=10;
+console.log(a);
+a=12;
+console.log(a);
+a=10;
+console.log(a);
+let b=7*a;
+console.log(b);
+const max=57;
+const actual=max-13;
+const percentage=actual/max;
+console.log(percentage)
+console.log(max===-max-0);
