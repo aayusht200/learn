94a986fddf5c52db044965f9717c7d8848b3315c
aayusht200 <aayush.t200@gmail.com>
2025-06-05 20:03:55 -0400

    assignment 2 done for filters

--- Javascript/Scope/arrayFilterAssignmentTwo.js
+++ Javascript/Scope/arrayFilterAssignmentTwo.js
@@ -18,3 +18,50 @@ function vowel(arr) {
   return arr.filter((word) => isVowel(word[0].toLowerCase()));
 }
 
+function uniqueElements(arr) {
+  return new Set(
+    arr.filter((value, index) => {
+      return arr.indexOf(value) == index;
+    })
+  );
+  // return arr.filter((value) => {
+  //   return arr.indexOf(value);
+  // });
+}
+
+function validEmail(arr) {
+  return arr.filter((val) => {
+    return val.includes("@") && val.includes(".");
+  });
+}
+
+function notNull(arr) {
+  return arr.filter(Boolean);
+}
+
+function arrRange(arr, a, b) {
+  return arr.filter((value) => {
+    return value >= a && value <= b;
+  });
+}
+
+function adminVerify(arr) {
+  return arr.filter((value) => value.role == "admin");
+}
+function stringReverse(name) {
+  return name.toString().split("").reverse().join("");
+}
+
+function palindrome(arr) {
+  return arr.filter(
+    (val) => val.toString().split("").reverse().join("") === val.toString()
+  );
+}
+
+function inStock(arr) {
+  return arr.filter((val) => val.inStock);
+}
+
+function evenIndex(arr) {
+  return arr.filter((val, index) => index % 2 == 0);
+}
