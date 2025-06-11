d92d73b8d47fc1b00608858bacee49a164005631
aayusht200 <aayush.t200@gmail.com>
2025-06-11 10:45:01 -0400

    almost done need to learn question 11 but have the concepct in mind bye

--- DOM Model/dommanipulation.js
+++ DOM Model/dommanipulation.js
@@ -1,11 +1,29 @@
 const header = document.getElementsByClassName("dom-header");
 console.log(header[0]);
+
 const sect = document.createElement("section");
 sect.classList.add("container");
+
 const headerTwo = document.createElement("h2");
 headerTwo.textContent = "Welcome to my site!";
 sect.appendChild(headerTwo);
+
 const content = document.querySelector(".dom");
 content.appendChild(sect);
-content.style.backgroundColor = "#f0f0f0";
+
+// content.style.backgroundColor = "#f0f0f0";
 sect.style.padding = "2rem";
+
+const btn = document.createElement("button");
+btn.textContent = "Toogle Theme";
+sect.appendChild(btn);
+
+btn.addEventListener("click", () => {
+  content.classList.toggle("dark-theme");
+  if (headerTwo.textContent === "Welcome to my site!") {
+    headerTwo.textContent = "You toggled the theme!";
+  } else {
+    headerTwo.textContent = "Welcome to my site!";
+  }
+});
+
